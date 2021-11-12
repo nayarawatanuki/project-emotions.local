148f3dfa8e769f90fbf09a9fba5417313ebce75c
nayarawatanuki <nayarawatanuki@gmail.com>
2021-11-12 00:28:54 -0300

    project save

--- src/backend/db/migrations/20211110191830-rewards.js
+++ src/backend/db/migrations/20211110191830-rewards.js
@@ -2,19 +2,12 @@
 
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    await queryInterface.createTable('kid_tasks', { 
+    await queryInterface.createTable('rewards', { 
       id: {
         type: Sequelize.INTEGER,
         primaryKey: true,
         autoIncrement: true,
         allowNull: false,
-      },
-      kid_id: {
-        type: Sequelize.INTEGER,
-        allowNull: false,
-        references: { model: 'kids', key: 'id' },
-        onUpdate: 'CASCADE',
-        onDelete: 'CASCADE',
       },
       task_id: {
         type: Sequelize.INTEGER,
@@ -22,6 +15,15 @@ module.exports = {
         references: { model: 'tasks', key: 'id' },
         onUpdate: 'CASCADE',
         onDelete: 'CASCADE',
+      },
+      photo: {
+        type: Sequelize.STRING,
+        unique: true,
+        allowNull: false,
+      },
+      message: {
+        type: Sequelize.STRING,
+        allowNull: false,
       },
       createdAt: {
         type: Sequelize.DATE,
@@ -35,6 +37,6 @@ module.exports = {
   },
 
   down: async (queryInterface, Sequelize) => {
-    await queryInterface.dropTable('kid_tasks');
+    await queryInterface.dropTable('rewards');
   }
 };
--- src/backend/server.js
+++ src/backend/server.js
@@ -9,6 +9,7 @@ const routesAdmin = require('./routes/Admins');
 const routesKid = require('./routes/Kids');
 const routesTask = require('./routes/Tasks');
 const routesTaskView = require('./routes/TaskView');
+const routesTaskReward = require('./routes/Rewards');
 const routesTaskResult = require('./routes/Results');
 
 const app = express();
@@ -29,6 +30,7 @@ app.use(routesAdmin);
 app.use(routesKid);
 app.use(routesTask);
 app.use(routesTaskView);
+app.use(routesTaskReward);
 app.use(routesTaskResult);
 
 app.listen(3000, () => {
